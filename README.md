b3bc0fe6cfce7e831319517e75551ecbd68889a3
Samuel S. Watson <samuel.s.watson@gmail.com>
2024-02-19 09:02:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# social-money-network
+
+Check out the `.ipynb` files in the `notebooks` directory for details.
+
+Describe your project here.
+* License: MIT
---
+++ notebooks/config.py
@@ -0,0 +1,7 @@
+
+user_db = "PL_DEMO"
+role = "RAI_INTEGRATION_CONSUMER"
+native_app_db = "NA_APP_WITH_STAGING_VERSIONS_APP"
+user_db = "PL_DEMO"
+warehouse = "PATRICK_DEMO"
+schema = "PUBLIC"
